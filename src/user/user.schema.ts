ca06b3cd0acde056018a7379b83b3500f6c87196
omar abdelwhab <omarabdelwhab20@gmail.com>
2025-03-19 14:57:24 +0200

    created the User schema

--- src/user/user.schema.ts
+++ src/user/user.schema.ts
@@ -3,10 +3,92 @@ import { HydratedDocument } from 'mongoose';
 
 export type UserDocument = HydratedDocument<User>;
 
-@Schema()
+@Schema({timestamps : true})
 export class User {
-  @Prop()
+  @Prop({
+    required : true,
+    type : String,
+    min : [3 , "Name must be at least 3 characters"],
+    max : [30 , "Name must be at most 30 characters"],
+    
+  })
   name: string;
+
+  @Prop({
+    required : true,
+    type : String,
+    unique : true,
+  })
+  email: string;
+
+  @Prop({
+    required : true,
+    type : String,
+    min : [6 , "Name must be at least 6 characters"],
+    max : [30 , "Name must be at most 30 characters"],
+    
+  })
+  password: string;
+
+
+  @Prop({
+    type : String,
+    required : true,
+    default : 'user',
+    enum : ['user' , 'admin']
+  })
+  role : string
+
+
+
+  @Prop({
+    type : String
+  })
+  avatar : string
+
+
+  @Prop({
+    type : Number,
+  })
+  age : number
+
+  @Prop({
+    type : String,
+    unique : [true , "This phone number is already in use"]
+  })
+  phoneNumber : string
+
+
+  @Prop({
+    type : String ,
+  })
+  address : string
+
+
+  @Prop({
+    type : String ,
+    enum : ["true" , "false"]
+  })
+  active : string
+
+
+  @Prop({
+    type : String ,
+  })
+  verificationCode : string
+
+
+  @Prop({
+    type : String ,
+    enum : ["male" , "female"]
+  })
+  gender : string
+
+  
+
+
+
+
 
 }
 
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,9 +1,19 @@
-import { Injectable } from '@nestjs/common';
+import {  Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './user.schema';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
 
 @Injectable()
 export class UserService {
+
+  constructor(
+    @InjectModel(User.name)
+    private readonly UserModel :Model<User>
+  ){}
+
+
   create(createUserDto: CreateUserDto) {
     return 'This action adds a new user';
   }
